210d3b0ae2caedb500884017a616078b18e4086d
suhanlim <soohan2009@naver.com>
2024-03-13 00:41:17 +0900

    ✨ INIT: make boiler plate
    create Spring Boot project

---
+++ src/main/java/org/univ/dangol/DangolApplication.java
@@ -0,0 +1,13 @@
+package org.univ.dangol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/univ/dangol/DangolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.univ.dangol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
