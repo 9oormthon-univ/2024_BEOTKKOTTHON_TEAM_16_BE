7af092b7fc7dd9807fbf774785049846aacc51b7
D15M4S <122260287+D15M4S@users.noreply.github.com>
2024-03-22 15:18:57 +0900

    ✨FEAT : 북마크 기능 추가 (별을 누르면 등록, 별을 다시 누르면 삭제) +
    관련 DTO 및 서비스 생성
    
    # Conflicts:
    #	src/main/java/org/univ/dangol/controller/BookMarkController.java

--- src/main/java/org/univ/dangol/repository/BookMarkRepository.java
+++ src/main/java/org/univ/dangol/repository/BookMarkRepository.java
@@ -3,10 +3,11 @@ package org.univ.dangol.repository;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.univ.dangol.entity.BookMark;
+import org.univ.dangol.entity.Shop;
 import org.univ.dangol.entity.User;
 
 import java.util.Optional;
 
 public interface BookMarkRepository extends JpaRepository<BookMark, Long> {
-    @NonNull Optional<BookMark> findById(@NonNull Long id);
+    Optional<BookMark> findByUserAndShop(User user, Shop shop);
 }
