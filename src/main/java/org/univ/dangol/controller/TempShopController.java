9c595092df2edcf18d510649698c49d9fab941c6
suhanlim <soohan2009@naver.com>
2024-03-22 14:50:20 +0900

    FEAT: ShopController

--- src/main/java/org/univ/dangol/controller/TempShopController.java
+++ src/main/java/org/univ/dangol/controller/TempShopController.java
@@ -1,38 +1,46 @@
 package org.univ.dangol.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.univ.dangol.dto.BadgePosition;
 import org.univ.dangol.dto.ShopDTO;
 import org.univ.dangol.dto.ShopScreen;
+import org.univ.dangol.entity.Item;
+import org.univ.dangol.service.ItemService;
+import org.univ.dangol.service.ShopService;
 
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 public class TempShopController {
-    @GetMapping("users/{userId}/shops")
-    public ResponseEntity<ShopScreen> shopScreen(){
+    private final ShopService shopService;
+    private final ItemService itemService;
+    @GetMapping("users/{userId}/shopScreen")
+    public ShopScreen shopScreen(@PathVariable("userId") Long userId){
         // TO DO CHANGE REGION
-        List<ShopDTO> shopDTOList = new ArrayList<>();
-        BadgePosition badgePosition = BadgePosition.builder().build();
-         ShopScreen dto = ShopScreen.builder()
+        List<ShopDTO> shopDTOList = shopService.getShopDTOs();
+        Optional<Item> nextItem = itemService.getNextItem(userId);
+
+        //location Item 삭제, 모든 Item 에는 위치가 있음
+        BadgePosition badgePosition = nextItem.map(item -> {
+            Double longitude = Optional.ofNullable(item.getLongitude()).map(BigDecimal::doubleValue).orElse(null);
+            Double latitude = Optional.ofNullable(item.getLatitude()).map(BigDecimal::doubleValue).orElse(null);
+            return BadgePosition.builder()
+                    .longitude(longitude)
+                    .latitude(latitude)
+                    .build();
+        }).orElse(null); // nextItem이 없거나 longitude/latitude가 null인 경우 null을 반환
+
+        return ShopScreen.builder()
                  .shops(shopDTOList)
                  .badgePosition(badgePosition)
                  .build();
-        // END REGION
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(dto);
     }
 }
--- src/main/java/org/univ/dangol/service/ShopService.java
+++ src/main/java/org/univ/dangol/service/ShopService.java
@@ -6,7 +6,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.univ.dangol.dto.ShopDTO;
 import org.univ.dangol.entity.Shop;
 import org.univ.dangol.repository.ShopRepository;
+import org.univ.dangol.repository.UserItemRepository;
+import org.univ.dangol.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,21 +17,29 @@ import java.util.List;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ShopService {
-
     private final ShopRepository shopRepository;
     public static ShopDTO toShopDTO(Shop shop) {
-        ShopDTO shopDTO = ShopDTO.builder()
+        return ShopDTO.builder()
                 .id(shop.getId())
                 .name(shop.getName())
                 .address(shop.getAddress())
                 .imgUrl(shop.getImage())
                 .category(shop.getCategory())
-                .tags(Arrays.asList("Tag1", "Tag2"))
+                .tags(Arrays.asList(shop.getTag().split("-")))
                 .latitude(shop.getLatitude().doubleValue())
                 .longitude(shop.getLongitude().doubleValue())
                 .build();
-        return shopDTO;
     }
+    // 모든 Shop정보를 ShopDTO로 바꾸어 전달
+    public List<ShopDTO> getShopDTOs(){
+        List<Shop> shopList = getAllShops();
+        List<ShopDTO> ans = new ArrayList<>();
+        for (Shop shop : shopList) {
+            ans.add(toShopDTO(shop));
+        }
+        return ans;
+    }
+    @Transactional(readOnly = true)
     public List<Shop> getAllShops() {
         return shopRepository.findAll();
     }
