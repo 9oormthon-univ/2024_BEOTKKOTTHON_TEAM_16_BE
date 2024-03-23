38a763eb0a4779134847c03f29f70541261417bb
suhanlim <soohan2009@naver.com>
2024-03-24 00:03:52 +0900

    !!

--- src/main/java/org/univ/dangol/controller/OpenApiController.java
+++ src/main/java/org/univ/dangol/controller/OpenApiController.java
@@ -3,10 +3,12 @@ package org.univ.dangol.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.univ.dangol.dto.Position;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 
@@ -14,12 +16,13 @@ import java.math.BigDecimal;
 @RestController
 @AllArgsConstructor
 public class OpenApiController {
-    @PostMapping("/publicTraditional")
-    public String getTraditionalShopDateByUserPosition(@RequestBody Position position){
+    private final WebClient webClient;
+    @GetMapping("/publicTraditional")
+    public Mono<String> getMarketDateByUserPosition(@RequestBody Position position){
         BigDecimal latitude = position.getLatitude();
         BigDecimal longitude = position.getLongitude();
         String ans = latitude.toString()+longitude.toString();
 
-        return ans;
+        return "test";
     }
 }
